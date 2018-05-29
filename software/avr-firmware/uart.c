ccff4a3e946db99fc883abf23920a520a8bfd6c1
newHeiko <35038740+newHeiko@users.noreply.github.com>
2018-05-29 12:54:17 +0200

    Added uart handing code (except HW)
    - Added TX queue handling
    - Added buffering input data from RX
    - TBD: Hardware initialization and adaptation to 328
    - TBD: Interpreting received strings

---
+++ software/avr-firmware/uart.c
@@ -0,0 +1,118 @@
+/**
+ * This file is part of the wiFred wireless throttle project
+ *
+ * It provides functions for writing all key and speed information to the RS232 port and for reading LED values from RS232 port
+ *
+ * (c) 2018 Heiko Rosemann
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "uart.h"
+
+#define BAUD 115200
+#include <util/setbaud.h>
+
+/**
+ * Initialize UART for 115200 8N1, RXC interrupt
+ */
+void initUART(void)
+{
+}
+
+/**
+ * Buffer for UART queues
+ */
+volatile char txBuffer[TX_BUFFER_SIZE];
+volatile char rxBuffer[RX_BUFFER_SIZE];
+
+volatile bool rxDone = false;
+
+/**
+ * Indexes for UART TX queue
+ */
+volatile uint8_t readIndex = 0, writeIndex = 0;
+
+/**
+ * Enqueue data to be sent
+ */
+void uartSendData(uint8_t * data, uint8_t length)
+{
+  while(length-- != 0)
+    {
+      txBuffer[writeIndex++] = *data++;
+      if(writeIndex >= TX_BUFFER_SIZE)
+	{
+	  writeIndex = 0;
+	}
+    }
+
+  // check if UART is active
+  if(! (UCSRB & (1<<UDRIE)) )
+    {
+      // transmit first byte if not
+      UCSRB |= (1<<UDRIE);
+      UDR = txBuffer[readIndex++];
+      if(readIndex >= TX_BUFFER_SIZE)
+	{
+	  readIndex = 0;
+	}
+    }
+}
+
+/**
+ * UDR empty interrupt - send next byte, if expected
+ */
+ISR(USART_UDRE_vect)
+{
+  if(readIndex != writeIndex)
+    {
+      UDR = txBuffer[readIndex++];
+      if(readIndex >= TX_BUFFER_SIZE)
+	{
+	  readIndex = 0;
+	}
+    }
+  else
+    {
+      UCSRB &= ~(1<<UDRIE);
+    }
+}
+
+ISR(USART_RXC_vect)
+{
+  char data = UDR;
+
+  static uint8_t rx_in = 0;
+
+  if(data == '\n' || data == '\r')
+    {
+      // check if any data has been received before
+      if(rx_in != 0)
+	{
+	  // reception has been completed
+	  rxDone = true;
+	  if(rx_in < RX_BUFFER_SIZE)
+	    {	      
+	      rxBuffer[rx_in] = 0;
+	    }
+	  else
+	    {
+	      rxBuffer[RX_BUFFER_SIZE - 1] = 0;
+	    }
+	  rx_in = 0;
+	}
+    }
+  else
+    {
+      // guard against buffer overflow
+      if(rx_in < RX_BUFFER_SIZE)
+	{
+	  rxBuffer[rx_in] = data;
+	}
+      rx_in++;
+    }
+  
+}
---
+++ software/avr-firmware/uart.h
@@ -0,0 +1,31 @@
+/**
+ * This file is part of the wiFred wireless throttle project
+ *
+ * It provides functions for writing all key and speed information to the RS232 port and for reading LED values from RS232 port
+ *
+ * (c) 2018 Heiko Rosemann
+ */
+
+#ifndef _UART_H_
+#define _UART_H_
+
+#include <stdint.h>
+#include <avr/io.h>
+
+/**
+ * Size of UART TX and RX buffers
+ */
+#define TX_BUFFER_SIZE 64
+#define RX_BUFFER_SIZE 64
+
+/**
+ * Initialize UART for 115200 8N1, RXC interrupt
+ */
+void initUART(void);
+
+/**
+ * Enqueue data to be sent
+ */
+void uartSendData(uint8_t * data, uint8_t length);
+
+#endif
