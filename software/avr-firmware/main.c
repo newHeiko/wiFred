4c047e742a3643ea693b47ec0f35d24474302fc0
newHeiko <35038740+newHeiko@users.noreply.github.com>
2019-03-04 22:08:48 +0100

    Yet another off-by-one error, this one for voltage

--- software/avr-firmware/main.c
+++ software/avr-firmware/main.c
@@ -80,9 +80,9 @@ int main(void)
 	    }
 	  uartSendData(buffer, sizeof("S:100:F\r\n") - 1);
 
-	  snprintf(buffer, sizeof("Vxxxxx\r\n"), "V:%04u\r\n", getBatteryVoltage());
-	  uartSendData(buffer, sizeof("Vxxxxx\r\n"));
-
+	  snprintf(buffer, sizeof("V:1234\r\n"), "V:%04u\r\n", getBatteryVoltage());
+	  uartSendData(buffer, sizeof("V:1234\r\n") - 1);
+	  
 	  if(getBatteryVoltage() < LOW_BATTERY_VOLTAGE)
 	    {
 	      uartSendData("BLOW\r\n", sizeof("BLOW\r\n") - 1);
