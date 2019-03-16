28bfe2412a8355303a3583a27e7874ba04a090e1
newHeiko <35038740+newHeiko@users.noreply.github.com>
2019-03-16 12:38:24 +0100

    Changed documentation generation process
    - Automatically convert bitmap files to EPS
    - Reduce bitmap file size during conversion
    - Fix HTML documentation generation

--- documentation/createDocs.sh
+++ documentation/createDocs.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+echo "Creating eps files from image files"
+
+for i in images/*jpg
+do
+	convert $i -resize 1506x1506\> -quality 50 -density 72 `dirname $i`/`basename $i .jpg`.eps
+done
+
+for i in images/*png
+do
+	convert  $i -resize 1506x1506\> -quality 50 -density 72 `dirname $i`/`basename $i .png`.eps
+done
+
 echo "Reducing eps image sizes"
 
 for i in images/*eps
 do
-	ps2ps $i $i.temp
+	ps2ps -dEPSFitPage $i $i.temp
 	mv $i.temp $i
 done
 
@@ -15,7 +27,9 @@ gs -sDEVICE=pdfwrite -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile
 mv docu.pdf.temp docu.pdf
 latex docu.tex
 latex docu.tex
+cp docu.tex html/
 cd html
-imagen ../docu.tex
-hevea article.hva ../docu.tex
+hevea -fix -s article.hva docu.tex
+imagen docu
+rm docu.tex
 cd ..
