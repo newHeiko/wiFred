newHeiko/WiFi-JMRI-Clock

Add http firmware updater

software/esp-firmware/arduino_main_sketch.ino/wifi.cpp
@@ -22,6 +22,7 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
+#include <ESP8266HTTPUpdateServer.h>
 #include <ESP8266mDNS.h>
 #include <DNSServer.h>
 
@@ -38,6 +39,7 @@ t_wlan wlan;
 
 ESP8266WebServer server(80);
 DNSServer dnsServer;
+ESP8266HTTPUpdateServer updater;
 
 void readString(char * dest, size_t maxLength, String input)
 {
@@ -199,7 +201,9 @@ void writeMainPage()
   resp        += String("<hr>Restart system to enable new WiFi settings<hr>\r\n")
                  + "<a href=restart.html>Restart system to enable new WiFi settings</a>\r\n"
                  + "<hr><hr>Status page<hr>\r\n"
-                 + "<a href=status.html>wiFred status subpage</a>\r\n"
+                 + "<a href=status.html>wiClock status subpage</a>\r\n"
+                 + "<hr><hr>Update firmware<hr>\r\n"
+                 + "<a href=update>Update wiClock firmware</a>\r\n"
                  + "</body></html>";
   server.send(200, "text/html", resp);
 }
@@ -288,6 +292,8 @@ void initWiFi(void)
   server.on("/status.html", writeStatusPage);
   server.on("/restart.html", restartESP);
   server.onNotFound(writeMainPage);
+
+  updater.setup(&server);
 
   // start configuration webserver
   server.begin();

newHeiko/wiFred

(cherry picked from commit 1b427c3c423af10f54866c01d80060a1ed94ac59)

software/esp-firmware/arduino_main_sketch.ino/wifi.cpp
@@ -22,6 +22,7 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
+#include <ESP8266HTTPUpdateServer.h>
 #include <ESP8266mDNS.h>
 #include <DNSServer.h>
 
@@ -37,8 +38,9 @@
 
 t_wlan wlan;
 
-DNSServer dnsServer;
 ESP8266WebServer server(80);
+DNSServer dnsServer;
+ESP8266HTTPUpdateServer updater;
 
 void readString(char * dest, size_t maxLength, String input)
 {
@@ -56,11 +58,15 @@ void handleWiFi(void)
       // intentional fall-through
 
     case STATE_CONNECTED:
+    case STATE_LOCO_CONNECTING:
+    case STATE_LOCO_ONLINE:
     case STATE_CONFIG_STATION:
     case STATE_CONFIG_STATION_WAITING:
       MDNS.update();
       break;
 
+    case STATE_LOWPOWER_WAITING:
+    case STATE_LOWPOWER:
     case STATE_STARTUP:
     case STATE_CONNECTING:
       break;
@@ -205,6 +211,8 @@ void writeMainPage()
               + "<a href=restart.html>Restart system to enable new WiFi settings</a>\r\n"
               + "<hr><hr>Status page<hr>\r\n"
               + "<a href=status.html>wiFred status subpage</a>\r\n"
+              + "<hr><hr>Update firmware<hr>\r\n"
+              + "<a href=update>Update wiFred firmware</a>\r\n"
               + "</body></html>";
   server.send(200, "text/html", resp);
 }
@@ -336,7 +344,9 @@ void initWiFi(void)
   server.on("/status.html", writeStatusPage);
   server.on("/restart.html", restartESP);
   server.onNotFound(writeMainPage);
-  
+
+  updater.setup(&server);
+
   // start configuration webserver
   server.begin();
 }