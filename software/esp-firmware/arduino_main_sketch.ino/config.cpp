newHeiko/WiFi-JMRI-Clock

May not compile: Moved configuration to SPIFFS...
...from EEPROM

- Also re-jiggled configuration websites
- Also allowed for more granular configuration saving

software/esp-firmware/arduino_main_sketch.ino/config.cpp
@@ -20,54 +20,27 @@
  */
 
 #include <stdbool.h>
+#include <FS.h>
+#include <ArduinoJson.h>
 
 #include "config.h"
 #include "wifi.h"
 #include "clockHandling.h"
 
-bool wifiSaved = false;
-bool clockSaved = false;
-
-char throttleName[NAME_CHARS];
-
-void eepromReadBlock(uint8_t * dest, const uint16_t src, size_t length)
-{
-  uint16_t local_src;
-  for(local_src = src; local_src < src + length; dest++, local_src++)
-  {
-    *dest = EEPROM.read(local_src);
-  }
-}
-
-void eepromWriteBlock(const uint16_t dest, uint8_t * src, size_t length)
-{
-  uint16_t local_dest;
-  for(local_dest = dest; local_dest < dest + length; local_dest++, src++)
-  {
-    EEPROM.write(local_dest, *src);
-  }
-}
+char * throttleName;
 
 /*
  * Read all configuration from EEPROM
  */
 void initConfig(void)
 {
-  EEPROM.begin(512);
-
-  // check for valid EEPROM data
-  uint8_t idClock = EEPROM.read(ID_CLOCK);
-
-// initialize to default values
-  memcpy(wlan.ssid, "undef", sizeof("undef"));
-  memcpy(wlan.key, "undef", sizeof("undef"));
-
   uint8_t mac[6];
   WiFi.macAddress(mac);
   String tN = "wiClock-" + String(mac[0], 16) + String(mac[5], 16);
-  memcpy(throttleName, tN.c_str(), tN.length() + 1);
+  throttleName = strdup(tN.c_str());
 
-  memcpy(clockServer.name, "undef", sizeof("undef"));
+  clockServer.automatic = true;
+  clockServer.name = strdup("undef");
   clockServer.port = 12080;
 
   startupTime.hours = 5;
@@ -77,54 +50,261 @@ void initConfig(void)
   clockMaxRate = 10;
   clockPulseLength = 40;
   clockOffset = 2;
-
   
-  if(idClock == EEPROM_VALID) // general EEPROM data is valid
+  if(!SPIFFS.begin())
   {
-    eepromReadBlock((uint8_t *) wlan.ssid, WLAN_SSID, sizeof(wlan.ssid)/sizeof(wlan.ssid[0]));
-    wlan.ssid[sizeof(wlan.ssid)/sizeof(wlan.ssid[0]) - 1] = '\0';
-    eepromReadBlock((uint8_t *) wlan.key, WLAN_KEY, sizeof(wlan.key)/sizeof(wlan.key[0]));
-    wlan.key[sizeof(wlan.key)/sizeof(wlan.key[0]) - 1] = '\0';
-    eepromReadBlock((uint8_t *) throttleName, NAME, sizeof(throttleName)/sizeof(throttleName[0]));
-    throttleName[sizeof(throttleName)/sizeof(throttleName[0]) - 1] = '\0';
-    wifiSaved = true;
-    clockMaxRate = EEPROM.read(CLOCK_MAX_RATE);
-    clockPulseLength = EEPROM.read(CLOCK_PULSE_LENGTH);
-    clockOffset = (int8_t) EEPROM.read(CLOCK_OFFSET);
-    eepromReadBlock((uint8_t *) &startupTime, CLOCK_STARTUP, sizeof(startupTime));
-    eepromReadBlock((uint8_t *) &clockServer, CLOCK_SERVER, sizeof(clockServer));
-    clockSaved = true;
+    return;
   }
+  
+  DynamicJsonDocument doc(256);
+
+// read device name from SPIFFS
+  if(File f = SPIFFS.open(FN_NAME, "r"))
+  {
+    if(!deserializeJson(doc, f))
+    {
+      const char * s = doc[FIELD_NAME_NAME];
+      if(s != nullptr)
+      {
+        free(throttleName);
+        throttleName = strdup(s);
+      }
+    }
+    f.close();
+  }
+
+// read server configuration/information from SPIFFS
+  if(File f = SPIFFS.open(FN_SERVER, "r"))
+  {
+    if(!deserializeJson(doc, f))
+    {
+      const char * s = doc[FIELD_SERVER_NAME];
+      if(s != nullptr)
+      {
+        free(clockServer.name);
+        clockServer.name = strdup(s);
+      }
+      uint16_t p = doc[FIELD_SERVER_PORT];
+      if(p != 0)
+      {
+        clockServer.port = p;
+      }
+      if(doc.containsKey(FIELD_SERVER_AUTOMATIC))
+      {
+        clockServer.automatic = doc[FIELD_SERVER_AUTOMATIC];
+      }
+    }
+    f.close();
+  }
+
+// read startup time from SPIFFS
+  if(File f = SPIFFS.open(FN_STARTUP_TIME, "r"))
+  {
+    if(!deserializeJson(doc, f))
+    {
+      if( doc.containsKey(FIELD_STARTUP_HOUR) && doc.containsKey(FIELD_STARTUP_MINUTE) 
+       && doc.containsKey(FIELD_STARTUP_SECOND) && doc.containsKey(FIELD_STARTUP_RATE) )
+      {
+        startupTime.hours = doc[FIELD_STARTUP_HOUR];
+        startupTime.minutes = doc[FIELD_STARTUP_MINUTE];
+        startupTime.seconds = doc[FIELD_STARTUP_SECOND];
+        startupTime.rate10 = doc[FIELD_STARTUP_RATE];
+      }
+    }
+    f.close();
+  }
+
+// read clock configuration from SPIFFS
+  if(File f = SPIFFS.open(FN_CLOCK_CONFIG, "r"))
+  {
+    if(!deserializeJson(doc, f))
+    {
+      if(doc.containsKey(FIELD_CLOCK_MAXRATE))
+      {
+        clockMaxRate = doc[FIELD_CLOCK_MAXRATE];
+      }
+      if(doc.containsKey(FIELD_CLOCK_PULSELENGTH))
+      {
+        clockPulseLength = doc[FIELD_CLOCK_PULSELENGTH];
+      }
+      if(doc.containsKey(FIELD_CLOCK_OFFSET))
+      {
+        clockOffset = doc[FIELD_CLOCK_OFFSET];
+      }
+    }
+    f.close();
+  }
+
+// read as many wifi configurations as there are available from SPIFFS  
+  String filename;
+
+  for(uint8_t i = 0; true; i++)
+  {
+    // create new filename
+    filename = String(FN_WIFI_STUB) + i + ".txt";
+    // check if it exists, exit loop if not
+    if(!SPIFFS.exists(filename))
+    {
+      break;
+    }
+
+    if(File f = SPIFFS.open(filename, "r"))
+    {
+      if(!deserializeJson(doc, f))
+      {
+        wifiAPEntry newAP;
+        newAP.ssid = strdup(doc[FIELD_WIFI_SSID] | "");
+        newAP.key = strdup(doc[FIELD_WIFI_PSK] | "");
+  
+        if(strcmp(newAP.ssid, ""))
+        {
+          apList.push_back(newAP);
+        }
+        else
+        {
+          free(newAP.ssid);
+          free(newAP.key);
+        }
+      }
+      f.close();
+    }
+  }
+
+  SPIFFS.end();
+}
+
+void saveClockServer()
+{
+  if(!SPIFFS.begin())
+  {
+    return;
+  }
+
+  DynamicJsonDocument doc(256);
+
+  doc[FIELD_SERVER_NAME] = clockServer.name;
+  doc[FIELD_SERVER_PORT] = clockServer.port;
+  doc[FIELD_SERVER_AUTOMATIC] = clockServer.automatic;
+
+  if(File f = SPIFFS.open(FN_SERVER, "w"))
+  {
+    serializeJson(doc, f);
+    f.close();
+  }
+
+  SPIFFS.end();
+}
+
+void saveClockStartup()
+{
+  if(!SPIFFS.begin())
+  {
+    return;
+  }
+
+  DynamicJsonDocument doc(256);
+
+  doc[FIELD_STARTUP_HOUR] = startupTime.hours;
+  doc[FIELD_STARTUP_MINUTE] = startupTime.minutes;
+  doc[FIELD_STARTUP_SECOND] = startupTime.seconds;
+  doc[FIELD_STARTUP_RATE] = startupTime.rate10;
+
+  if(File f = SPIFFS.open(FN_STARTUP_TIME, "w"))
+  {
+    serializeJson(doc, f);
+    f.close();
+  }
+
+  SPIFFS.end();
+ 
 }
 
 void saveClockConfig()
 {
-  EEPROM.write(CLOCK_MAX_RATE, clockMaxRate);
-  EEPROM.write(CLOCK_PULSE_LENGTH, clockPulseLength);
-  EEPROM.write(CLOCK_OFFSET, clockOffset);
-
-  eepromWriteBlock(CLOCK_STARTUP, (uint8_t *) &startupTime, sizeof(startupTime));
-  eepromWriteBlock(CLOCK_SERVER, (uint8_t *) &clockServer, sizeof(clockServer));
-
-  clockSaved = true;
-  
-  if(wifiSaved)
+  if(!SPIFFS.begin())
   {
-    EEPROM.write(ID_CLOCK, EEPROM_VALID);
+    return;
   }
-  EEPROM.commit();
+
+  DynamicJsonDocument doc(256);
+
+  doc[FIELD_CLOCK_MAXRATE] = clockMaxRate;
+  doc[FIELD_CLOCK_PULSELENGTH] = clockPulseLength;
+  doc[FIELD_CLOCK_OFFSET] = clockOffset;
+
+  if(File f = SPIFFS.open(FN_CLOCK_CONFIG, "w"))
+  {
+    serializeJson(doc, f);
+    f.close();
+  }
+
+  SPIFFS.end();
 }
 
 void saveGeneralConfig()
 {
-  eepromWriteBlock(WLAN_SSID, (uint8_t *) wlan.ssid, sizeof(wlan.ssid)/sizeof(wlan.ssid[0]));
-  eepromWriteBlock(WLAN_KEY, (uint8_t *) wlan.key, sizeof(wlan.key)/sizeof(wlan.key[0]));
-  eepromWriteBlock(NAME, (uint8_t *) throttleName, sizeof(throttleName)/sizeof(throttleName[0]));
-
-  wifiSaved = true;
-  if(clockSaved)
+  if(!SPIFFS.begin())
   {
-    EEPROM.write(ID_CLOCK, EEPROM_VALID);
+    return;
   }
-  EEPROM.commit();
+
+  DynamicJsonDocument doc(256);
+
+  doc[FIELD_NAME_NAME] = throttleName;
+
+  if(File f = SPIFFS.open(FN_NAME, "w"))
+  {
+    serializeJson(doc, f);
+    f.close();
+  }
+
+  SPIFFS.end();
+}
+
+void saveWiFiConfig()
+{
+  if(!SPIFFS.begin())
+  {
+    return;
+  }
+  
+  DynamicJsonDocument doc(256);
+
+  String filename;
+
+  for(uint8_t i = 0; ; i++)
+  {
+    // create new filename
+    filename = String(FN_WIFI_STUB) + i + ".txt";
+    // check if it exists, exit loop if not
+    if(!SPIFFS.exists(filename))
+    {
+      break;
+    }
+    SPIFFS.remove(filename);
+  }
+
+  uint8_t i = 0;
+  for(std::vector<wifiAPEntry>::iterator it = apList.begin(); it != apList.end(); it++, i++)
+  {
+    // create new filename
+    filename = String(FN_WIFI_STUB) + i + ".txt";
+
+    doc[FIELD_WIFI_SSID] = it->ssid;
+    doc[FIELD_WIFI_PSK] = it->key;
+
+    if(File f = SPIFFS.open(filename, "w"))
+    {
+      serializeJson(doc, f);
+      f.close();
+    }
+    doc.clear();
+  }
+
+  SPIFFS.end();
+}
+
+void deleteAllConfig()
+{
+  SPIFFS.format();
 }

newHeiko/wiFred

software/esp-firmware/arduino_main_sketch.ino/config.cpp
@@ -20,53 +20,28 @@
  */
 
 #include <stdbool.h>
+#include <FS.h>
+#include <ArduinoJson.h>
 
 #include "config.h"
 #include "wifi.h"
 #include "locoHandling.h"
 
-bool wifiSaved = false;
-bool locoSaved = false;
-
-char throttleName[NAME_CHARS];
-
-void eepromReadBlock(uint8_t * dest, const uint16_t src, size_t length)
-{
-  uint16_t local_src;
-  for(local_src = src; local_src < src + length; dest++, local_src++)
-  {
-    *dest = EEPROM.read(local_src);
-  }
-}
-
-void eepromWriteBlock(const uint16_t dest, uint8_t * src, size_t length)
-{
-  uint16_t local_dest;
-  for(local_dest = dest; local_dest < dest + length; local_dest++, src++)
-  {
-    EEPROM.write(local_dest, *src);
-  }
-}
+char * throttleName;
 
 /*
  * Read all configuration from EEPROM
  */
 void initConfig(void)
 {
-  EEPROM.begin(512);
-
-  // check for valid EEPROM data
-  uint8_t idLoco = EEPROM.read(ID_LOCOS);
-
 // initialize to default values
-  memcpy(wlan.ssid, "undef", sizeof("undef"));
-  memcpy(wlan.key, "undef", sizeof("undef"));
-
   uint8_t mac[6];
   WiFi.macAddress(mac);
   String tN = "wiFred-" + String(mac[0], 16) + String(mac[5], 16);
   memcpy(throttleName, tN.c_str(), tN.length() + 1);
-  memcpy(locoServer.name, "undef", sizeof("undef"));
+
+  locoServer.automatic = true;
+  locoServer.name = strdup("undef");
   locoServer.port = 12090;
 
   for(int i=0; i<4; i++)
@@ -74,55 +49,199 @@ void initConfig(void)
     locos[i].address = -1;
     locos[i].reverse = false;
     locos[i].longAddress = true;
+    for(int j=0; j<MAX_FUNCTION + 1; j++)
+      {
+	locos[i].functions[j] = THROTTLE;
+      }
   }
   
-  if(idLoco == EEPROM_VALID) // general EEPROM data is valid
+  if(!SPIFFS.begin())
   {
-    eepromReadBlock((uint8_t *) wlan.ssid, WLAN_SSID, sizeof(wlan.ssid)/sizeof(wlan.ssid[0]));
-    wlan.ssid[sizeof(wlan.ssid)/sizeof(wlan.ssid[0]) - 1] = '\0';
-    eepromReadBlock((uint8_t *) wlan.key, WLAN_KEY, sizeof(wlan.key)/sizeof(wlan.key[0]));
-    wlan.key[sizeof(wlan.key)/sizeof(wlan.key[0]) - 1] = '\0';
-    eepromReadBlock((uint8_t *) throttleName, NAME, sizeof(throttleName)/sizeof(throttleName[0]));
-    throttleName[sizeof(throttleName)/sizeof(throttleName[0]) - 1] = '\0';
-    wifiSaved = true;
-    eepromReadBlock((uint8_t *) &locoServer, LOCO_SERVER, sizeof(locoServer)); 
-    eepromReadBlock((uint8_t *) &(locos[0]), LOCO1, sizeof(locos[0]));
-    eepromReadBlock((uint8_t *) &(locos[1]), LOCO2, sizeof(locos[1]));
-    eepromReadBlock((uint8_t *) &(locos[2]), LOCO3, sizeof(locos[2]));
-    eepromReadBlock((uint8_t *) &(locos[3]), LOCO4, sizeof(locos[3]));
-    locoSaved = true;
+    return;
   }
-}
+  
+  DynamicJsonDocument doc(512);
 
-void saveGeneralConfig()
-{
-  eepromWriteBlock(WLAN_SSID, (uint8_t *) wlan.ssid, sizeof(wlan.ssid)/sizeof(wlan.ssid[0]));
-  eepromWriteBlock(WLAN_KEY, (uint8_t *) wlan.key, sizeof(wlan.key)/sizeof(wlan.key[0]));
-  eepromWriteBlock(NAME, (uint8_t *) throttleName, sizeof(throttleName)/sizeof(throttleName[0]));
-
-  wifiSaved = true;
-  if(locoSaved)
+// read device name from SPIFFS
+  if(File f = SPIFFS.open(FN_NAME, "r"))
   {
-    EEPROM.write(ID_LOCOS, EEPROM_VALID);
-  }
-  EEPROM.commit();
-}
-
-void saveLocoConfig(bool mainSave)
-{
-  eepromWriteBlock(LOCO_SERVER, (uint8_t *) &locoServer, sizeof(locoServer)); 
-  eepromWriteBlock(LOCO1, (uint8_t *) &(locos[0]), sizeof(locos[0]));
-  eepromWriteBlock(LOCO2, (uint8_t *) &(locos[1]), sizeof(locos[1]));
-  eepromWriteBlock(LOCO3, (uint8_t *) &(locos[2]), sizeof(locos[2]));
-  eepromWriteBlock(LOCO4, (uint8_t *) &(locos[3]), sizeof(locos[3]));
-
-  if(mainSave)
-  {
-    locoSaved = true;
-    if(wifiSaved)
+    if(!deserializeJson(doc, f))
     {
-      EEPROM.write(ID_LOCOS, EEPROM_VALID);
+      const char * s = doc[FIELD_NAME_NAME];
+      if(s != nullptr)
+      {
+        free(throttleName);
+        throttleName = strdup(s);
+      }
+    }
+    f.close();
+  }
+
+// read server configuration/information from SPIFFS
+  if(File f = SPIFFS.open(FN_SERVER, "r"))
+  {
+    if(!deserializeJson(doc, f))
+    {
+      const char * s = doc[FIELD_SERVER_NAME];
+      if(s != nullptr)
+      {
+        free(locoServer.name);
+        locoServer.name = strdup(s);
+      }
+      uint16_t p = doc[FIELD_SERVER_PORT];
+      if(p != 0)
+      {
+        locoServer.port = p;
+      }
+      if(doc.containsKey(FIELD_SERVER_AUTOMATIC))
+      {
+        locoServer.automatic = doc[FIELD_SERVER_AUTOMATIC];
+      }
+    }
+    f.close();
+  }
+
+// read as many wifi configurations as there are available from SPIFFS  
+  String filename;
+
+  for(uint8_t i = 0; true; i++)
+  {
+    // create new filename
+    filename = String(FN_WIFI_STUB) + i + ".txt";
+    // check if it exists, exit loop if not
+    if(!SPIFFS.exists(filename))
+    {
+      break;
+    }
+
+    if(File f = SPIFFS.open(filename, "r"))
+    {
+      if(!deserializeJson(doc, f))
+      {
+        wifiAPEntry newAP;
+        newAP.ssid = strdup(doc[FIELD_WIFI_SSID] | "");
+        newAP.key = strdup(doc[FIELD_WIFI_PSK] | "");
+  
+        if(strcmp(newAP.ssid, ""))
+        {
+          apList.push_back(newAP);
+        }
+        else
+        {
+          free(newAP.ssid);
+          free(newAP.key);
+        }
+      }
+      f.close();
     }
   }
-  EEPROM.commit();
+
+  // read four loco configurations from SPIFFS if available
+
+  SPIFFS.end();
+}
+
+void saveLocoServer()
+{
+  if(!SPIFFS.begin())
+  {
+    return;
+  }
+
+  DynamicJsonDocument doc(256);
+
+  doc[FIELD_SERVER_NAME] = locoServer.name;
+  doc[FIELD_SERVER_PORT] = locoServer.port;
+  doc[FIELD_SERVER_AUTOMATIC] = locoServer.automatic;
+
+  if(File f = SPIFFS.open(FN_SERVER, "w"))
+  {
+    serializeJson(doc, f);
+    f.close();
+  }
+
+  SPIFFS.end();
+}
+
+
+void saveGeneralConfig(void)
+{
+  if(!SPIFFS.begin())
+  {
+    return;
+  }
+
+  DynamicJsonDocument doc(256);
+
+  doc[FIELD_NAME_NAME] = throttleName;
+
+  if(File f = SPIFFS.open(FN_NAME, "w"))
+  {
+    serializeJson(doc, f);
+    f.close();
+  }
+
+  SPIFFS.end();
+}
+
+void saveLocoConfig(uint8_t loco)
+{
+  if(loco >= 4 || !SPIFFS.begin())
+  {
+    return;
+  }
+
+  DynamicJsonDocument doc(512);
+
+  // save loco configuration for current loco to SPIFFS
+
+  SPIFFS.end();
+}
+
+void saveWiFiConfig()
+{
+  if(!SPIFFS.begin())
+  {
+    return;
+  }
+  
+  DynamicJsonDocument doc(256);
+
+  String filename;
+
+  for(uint8_t i = 0; ; i++)
+  {
+    // create new filename
+    filename = String(FN_WIFI_STUB) + i + ".txt";
+    // check if it exists, exit loop if not
+    if(!SPIFFS.exists(filename))
+    {
+      break;
+    }
+    SPIFFS.remove(filename);
+  }
+
+  uint8_t i = 0;
+  for(std::vector<wifiAPEntry>::iterator it = apList.begin(); it != apList.end(); it++, i++)
+  {
+    // create new filename
+    filename = String(FN_WIFI_STUB) + i + ".txt";
+
+    doc[FIELD_WIFI_SSID] = it->ssid;
+    doc[FIELD_WIFI_PSK] = it->key;
+
+    if(File f = SPIFFS.open(filename, "w"))
+    {
+      serializeJson(doc, f);
+      f.close();
+    }
+    doc.clear();
+  }
+
+  SPIFFS.end();
+}
+
+void deleteAllConfig()
+{
+  SPIFFS.format();
 }