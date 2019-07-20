242268a76b6589f3dc68f013b77d8dc3138ec12b
newHeiko <35038740+newHeiko@users.noreply.github.com>
2019-07-21 01:57:12 +0200

    Only flush client if there is no transaction...
    ...currently in progress, acquiring locos or functions.

--- software/esp-firmware/arduino_main_sketch.ino/locoHandling.cpp
+++ software/esp-firmware/arduino_main_sketch.ino/locoHandling.cpp
@@ -160,7 +160,7 @@ void locoHandler(void)
           requestLocoFunctions(currentLoco);
           break;
         }
-        if(locoState[currentLoco] == LOCO_ACTIVATE && locos[currentLoco].address != -1)
+        else if(locoState[currentLoco] == LOCO_ACTIVATE && locos[currentLoco].address != -1)
         {
           // make sure no loco (currently attached) is moving
           setESTOP();
@@ -174,11 +174,15 @@ void locoHandler(void)
           client.print(String("MT-") + locoThrottleID[currentLoco] + "<;>" + locoThrottleID[currentLoco] + "\n");
           locoState[currentLoco] = LOCO_INACTIVE;
         }
+        else if(currentLoco == 3)
+        {
+          // if none of the locos had any status change,
+          // flush all input data
+          client.flush();
+          while (client.read() > -1)
+            ;
+        }
       }
-      // flush all input data
-      client.flush();
-      while (client.read() > -1)
-        ;
       break;
 
     default:
@@ -459,8 +463,18 @@ void requestLoco(uint8_t loco)
 void requestLocoFunctions(uint8_t loco)
 {
   String line = client.readStringUntil('\n');
+#ifdef DEBUG
+  Serial.println();
+  Serial.println(line);
+#endif
   if(line.startsWith(String("MTA") + locoThrottleID[loco]))
   {
+#ifdef DEBUG
+  Serial.println(line);
+  Serial.println(locoThrottleID[loco]);
+  Serial.println(String("") + loco + " " + line.charAt(6 + locoThrottleID[loco].length()));
+#endif
+
     bool set = false;
     uint8_t f = 0;
     
