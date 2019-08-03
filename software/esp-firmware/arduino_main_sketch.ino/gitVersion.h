7e43574bfea11662db5041df9cbabde0720ae685
newHeiko <35038740+newHeiko@users.noreply.github.com>
2019-08-03 22:31:29 +0200

    Add a shell script and a dummy file to get git rev
    Stored in a #define named REV
    
    (cherry picked from commit 8a57c2a775e3f4c9a8f7d0079ba44fe0e3efd3ad)

--- software/esp-firmware/arduino_main_sketch.ino/gitVersion.h
+++ software/esp-firmware/arduino_main_sketch.ino/gitVersion.h
@@ -1,1 +1,23 @@
+/**
+ * This file is part of the wiFred wireless model railroading throttle project
+ * Copyright (C) 2018  Heiko Rosemann
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>
+ *
+ * This file is a dummy which will be replaced at compile-time by the
+ * make-git-version script if properly setup. Including it will yield a 
+ * macro called REV with the format dateGithash, i.e. "2019-08-03e2aa239"
+ */
 
+ #define REV "unknown"
