e8f6b61162a4895ae3b48de35eeb5baa8b4b2289
newHeiko <35038740+newHeiko@users.noreply.github.com>
2019-12-23 15:23:33 +0100

    Add quickstart guide for pre-assembled PCBs

---
+++ documentation/createQuickstart.sh
@@ -0,0 +1,2 @@
+
+dblatex -d -P latex.output.revhistory=0 -P paper.type=a6paper -P doc.collab.show=0 -P doc.layout="title mainmatter" -P page.margin.bottom=5mm -P page.margin.top=5mm -Ppage.margin.inner=5mm -P page.margin.outer=5mm -P latex.class.options=notitlepage quickstart.xml 
