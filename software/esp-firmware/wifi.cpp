f2bebd0d14b4bfe5c42f0c2d51106cdfd5d70235
newHeiko <35038740+newHeiko@users.noreply.github.com>
2021-02-06 21:09:16 +0100

    Remove AVR firmware revision from configuration page

--- software/esp-firmware/wifi.cpp
+++ software/esp-firmware/wifi.cpp
@@ -347,8 +347,7 @@ void writeMainPage()
   resp        += String("<hr>wiFred status<hr>\r\n")
               + "<table border=0>"
               + "<tr><td>Battery voltage: </td><td>" + batteryVoltage + " mV" + (lowBattery ? " Battery LOW" : "" ) + "</td></tr>"
-              + "<tr><td>ESP firmware revision: </td><td>" + REV + "</td></tr>"
-              + "<tr><td>AVR firmware revision: </td><td>" + avrRevision + "</td></tr></table>\r\n"
+              + "<tr><td>Firmware revision: </td><td>" + REV + "</td></tr></table>\r\n"
               + "<hr>wiFred system<hr>\r\n"
               + "<a href=resetConfig.html>Reset wiFred to factory defaults</a>\r\n"
               + "<a href=update>Update wiFred firmware</a>\r\n"
