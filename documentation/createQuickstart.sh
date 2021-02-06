6cf697f28411786dc7c1d8ab0bca103a69b3c878
newHeiko <35038740+newHeiko@users.noreply.github.com>
2021-02-06 16:04:25 +0100

    Added quick start documentation...
    ...for drilljig and laser engraved housings

--- documentation/createQuickstart.sh
+++ documentation/createQuickstart.sh
@@ -1,2 +1,4 @@
-
-dblatex -d -P latex.output.revhistory=0 -P paper.type=a6paper -P doc.collab.show=0 -P doc.layout="title mainmatter" -P page.margin.bottom=5mm -P page.margin.top=5mm -Ppage.margin.inner=5mm -P page.margin.outer=5mm -P latex.class.options=notitlepage quickstart.xml 
+asciidoctor -b docbook quickstartLaser.adoc
+asciidoctor -b docbook quickstartDrilljig.adoc
+dblatex -P figure.warning="/etc/asciidoc/images/icons/warning" -P latex.output.revhistory=0 -P paper.type=a6paper -P doc.collab.show=0 -P doc.layout="title mainmatter" -P page.margin.bottom=2mm -P page.margin.top=5mm -P page.margin.inner=5mm -P page.margin.outer=5mm -P latex.class.options=notitlepage quickstartLaser.xml 
+dblatex -P figure.warning="/etc/asciidoc/images/icons/warning" -P latex.output.revhistory=0 -P paper.type=a6paper -P doc.collab.show=0 -P doc.layout="title mainmatter" -P page.margin.bottom=2mm -P page.margin.top=5mm -P page.margin.inner=5mm -P page.margin.outer=5mm -P latex.class.options=notitlepage quickstartDrilljig.xml 
