db3f4deb4eed95dddaaf931ccd806fdc5689f653
newHeiko <35038740+newHeiko@users.noreply.github.com>
2021-02-19 15:04:10 +0100

    Finished conversion to asciidoc
    Added large image and PDF handling

--- documentation/createDocs.sh
+++ documentation/createDocs.sh
@@ -1,45 +1,31 @@
 #!/bin/bash
 
+echo "Creating png files from pdf files"
+cd largeImages
+for i in *pdf
+do
+    PNGFILE=../images/`basename $i .pdf`.png
+    if [ $i -nt $PNGFILE ]
+    then
+	convert -density 150 $i $PNGFILE
+    fi
+done
+cd ..
+
+echo "Reducing jpg images in size"
+cd largeImages
+for i in *jpg
+do
+    if [ $i -nt ../images/$i ]
+    then
+	convert $i -resize 1920x1920 -quality 50 -strip ../images/$i
+    fi
+done
+cd ..
+
+echo "Creating html and pdf files"
 asciidoctor -b docbook docu.adoc
 asciidoctor -b html5 docu.adoc
 dblatex -P figure.note="/etc/asciidoc/images/icons/note" -P figure.important="/etc/asciidoc/images/icons/important" -P figure.warning="/etc/asciidoc/images/icons/warning" -P doc.collab.show=0 -P paper.type=a4paper -P page.margin.top=15mm -P page.margin.bottom=15mm -P page.margin.inner=20mm -P page.margin.outer=20mm docu.xml 
 
 exit
-
-echo "Creating eps files from image files and reducing sizes"
-
-for i in images/*jpg
-do
-	EPSFILE=`dirname $i`/`basename $i .jpg`.eps
-	if [ $i -nt $EPSFILE ]
-	then
-		convert $i -resize 1506x1506\> -quality 50 -density 72 $EPSFILE
-		ps2ps -dFitPage $EPSFILE $EPSFILE.temp
-		mv $EPSFILE.temp $EPSFILE
-	fi
-done
-
-for i in images/*png
-do
-	EPSFILE=`dirname $i`/`basename $i .png`.eps
-        if [ $i -nt $EPSFILE ]
-	then    
-                convert $i -resize 1506x1506\> -quality 50 -density 72 $EPSFILE
-                ps2ps -dFitPage $EPSFILE $EPSFILE.temp
-                mv $EPSFILE.temp $EPSFILE
-	fi      
-done
-
-echo "Creating all docs from latex source file"
-pdflatex docu.tex
-pdflatex docu.tex
-gs -sDEVICE=pdfwrite -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=docu.pdf.temp docu.pdf
-mv docu.pdf.temp docu.pdf
-latex docu.tex
-latex docu.tex
-cp docu.tex html/
-cd html
-hevea -fix -s article.hva docu.tex
-imagen docu
-rm docu.tex
-cd ..
