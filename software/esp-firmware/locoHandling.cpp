a04d9e009787753499fc3fb0dbc3f6df78cf6274
newHeiko <35038740+newHeiko@users.noreply.github.com>
2021-02-20 16:51:06 +0100

    Revert "Remove call to client.flush()"
    This reverts commit d0d50b67b0b6d91626fed427d2e3d976e6761402.
    
    Attempting to fix bug in old version...

--- software/esp-firmware/locoHandling.cpp
+++ software/esp-firmware/locoHandling.cpp
@@ -530,6 +530,7 @@ void requestLocoFunctions(uint8_t loco)
       case 's':
         locoState[loco] = LOCO_ACTIVE;
         // flush all input data
+        client.flush();
         while (client.read() > -1)
           ;
         break;
