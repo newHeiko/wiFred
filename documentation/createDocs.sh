5d7d2e66291981c50b66be8315fc1d8eb9a091ef
newHeiko <35038740+newHeiko@users.noreply.github.com>
2021-11-15 20:58:43 +0100

    Transfer translated html back to adoc

--- documentation/createDocs.sh
+++ documentation/createDocs.sh
@@ -24,8 +24,12 @@ done
 cd ..
 
 echo "Creating html and pdf files"
-asciidoctor -b docbook docu.adoc
-asciidoctor -b html5 docu.adoc
-dblatex -P figure.note="/etc/asciidoc/images/icons/note" -P figure.important="/etc/asciidoc/images/icons/important" -P figure.warning="/etc/asciidoc/images/icons/warning" -P doc.collab.show=0 -P paper.type=a4paper -P page.margin.top=15mm -P page.margin.bottom=15mm -P page.margin.inner=20mm -P page.margin.outer=20mm docu.xml 
+asciidoctor -b docbook docu_en.adoc
+asciidoctor -b docbook docu_de.adoc
+asciidoctor -b html5 docu_en.adoc
+asciidoctor -b html5 docu_de.adoc
+dblatex -P figure.note="/etc/asciidoc/images/icons/note" -P figure.important="/etc/asciidoc/images/icons/important" -P figure.warning="/etc/asciidoc/images/icons/warning" -P doc.collab.show=0 -P paper.type=a4paper -P page.margin.top=15mm -P page.margin.bottom=15mm -P page.margin.inner=20mm -P page.margin.outer=20mm docu_en.xml 
+dblatex -P figure.note="/etc/asciidoc/images/icons/note" -P figure.important="/etc/asciidoc/images/icons/important" -P figure.warning="/etc/asciidoc/images/icons/warning" -P doc.collab.show=0 -P paper.type=a4paper -P page.margin.top=15mm -P page.margin.bottom=15mm -P page.margin.inner=20mm -P page.margin.outer=20mm docu_de.xml
+
 
 exit
