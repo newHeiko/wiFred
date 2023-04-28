2e83c8e81f5ddeaaeb97c1025d74b60f61d9a9bc
newHeiko <35038740+newHeiko@users.noreply.github.com>
2023-04-28 17:32:10 +0200

    Automate file creation if input files are newer

--- documentation/createDocs.sh
+++ documentation/createDocs.sh
@@ -23,13 +23,30 @@ do
 done
 cd ..
 
-echo "Creating html and pdf files"
-asciidoctor -b docbook docu_en.adoc
-asciidoctor -b docbook docu_de.adoc
-asciidoctor -b html5 docu_en.adoc
-asciidoctor -b html5 docu_de.adoc
-dblatex -P figure.note="/etc/asciidoc/images/icons/note" -P figure.important="/etc/asciidoc/images/icons/important" -P figure.warning="/etc/asciidoc/images/icons/warning" -P doc.collab.show=0 -P paper.type=a4paper -P page.margin.top=15mm -P page.margin.bottom=15mm -P page.margin.inner=20mm -P page.margin.outer=20mm docu_en.xml 
-dblatex -P figure.note="/etc/asciidoc/images/icons/note" -P figure.important="/etc/asciidoc/images/icons/important" -P figure.warning="/etc/asciidoc/images/icons/warning" -P doc.collab.show=0 -P paper.type=a4paper -P page.margin.top=15mm -P page.margin.bottom=15mm -P page.margin.inner=20mm -P page.margin.outer=20mm docu_de.xml
+echo "Creating html files"
+for i in about assembly config docu esp12-schematic esp32-schematic leds server usage
+do
+	for j in de en
+	do
+		if [[ ${i}_$j.adoc -nt ${i}_$j.html ]] || [[ version_$j.adoc -nt ${i}_$j.html ]]
+		then
+			asciidoctor -b html5 ${i}_$j.adoc
+		fi
+	done
+done
 
+echo "Creating pdf files"
+for j in de en
+do
+	for i in about assembly config esp12-schematic esp32-schematic leds server usage pdfmaster version
+	do
+		if [[ ${i}_$j.adoc -nt pdfmaster_$j.pdf ]]
+		then
+			asciidoctor -b docbook5 pdfmaster_$j.adoc
+			dblatex -P figure.note="/etc/asciidoc/images/icons/note" -P figure.important="/etc/asciidoc/images/icons/important" -P figure.warning="/etc/asciidoc/images/icons/warning" -P doc.collab.show=0 -P paper.type=a4paper -P page.margin.top=15mm -P page.margin.bottom=15mm -P page.margin.inner=20mm -P page.margin.outer=20mm pdfmaster_$j.xml
+			break
+		fi
+	done
+done
 
 exit
